ac5f07f52364998a6ea39a0f0abeee1b16eef716
mayurtyagi <mayurtyagi@unolo>
2024-07-20 17:16:15 +0530

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Mayur on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
